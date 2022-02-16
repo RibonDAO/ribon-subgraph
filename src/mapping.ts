095743fc428bf5761b58ae6818704adea4527bc9
Nathiely Macedo <nathielymm@gmail.com>
2022-02-16 14:42:39 -0300

    fix: use plus function to sum BigInt on mapping functions

--- src/mapping.ts
+++ src/mapping.ts
@@ -15,63 +15,25 @@ import {
 } from "../generated/schema";
 
 export function handleDonationAdded(event: DonationAdded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
+
   let idDonation =
     event.params.user.toHex() +
     event.params.integration.toHex() +
     event.params.nonProfit.toHex();
   let entity = DonationBalance.load(idDonation);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (!entity) {
     entity = new DonationBalance(idDonation);
     entity.totalDonated = BigInt.fromI32(0);
   }
 
-  // BigInt and BigDecimal math are supported
-  // refactor to use BigInt
-  let entityBalance = entity.totalDonated;
-  let eventAmount = event.params.amount;
+  entity.totalDonated = entity.totalDonated.plus(event.params.amount);
 
-  let entityTotalDonated =
-    parseInt(`${entityBalance}`) + parseInt(`${eventAmount}`);
-
-  entity.totalDonated = BigInt.fromI32(entityTotalDonated);
-
-  // Entity fields can be set based on event parameters
   entity.user = event.params.user;
   entity.integration = event.params.integration;
   entity.nonProfit = event.params.nonProfit;
 
-  // Entities can be written to the store with `.save()`
   entity.save();
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.donationPoolBalance(...)
-  // - contract.donationToken(...)
-  // - contract.getIntegrationBalance(...)
-  // - contract.getIntegrationCouncil(...)
-  // - contract.integrationCouncil(...)
-  // - contract.integrations(...)
-  // - contract.isNonProfitOnWhitelist(...)
-  // - contract.nonProfitCouncil(...)
-  // - contract.nonProfits(...)
 }
 
 export function handleIntegrationBalanceUpdated(
@@ -85,14 +47,7 @@ export function handleIntegrationBalanceUpdated(
     entity.balance = BigInt.fromI32(0);
   }
 
-  // refactor to use BigInt
-  let entityBalance = entity.balance;
-  let eventAmount = event.params.amount;
-
-  let integrationBalance =
-    parseInt(`${entityBalance}`) + parseInt(`${eventAmount}`);
-
-  entity.balance = BigInt.fromI32(integrationBalance);
+  entity.balance = entity.balance.plus(event.params.amount);
 
   entity.save();
 }
@@ -132,14 +87,7 @@ export function handlePoolBalanceIncreased(event: PoolBalanceIncreased): void {
     entity.totalDonated = BigInt.fromI32(0);
   }
 
-  // refactor to use BigInt
-  let entityBalance = entity.totalDonated;
-  let eventAmount = event.params.amount;
-
-  let entityTotalDonated =
-    parseInt(`${entityBalance}`) + parseInt(`${eventAmount}`);
-
-  entity.totalDonated = BigInt.fromI32(entityTotalDonated);
+  entity.totalDonated = entity.totalDonated.plus(event.params.amount);
 
   entity.save();
 }
